3922274a06905af6624c0fccda6e00777f82f7ac
rheadley <rheadley@compcc.com>
2010-11-30 22:19:45 -0600

    added a mediochre deal method, mainly to just verify shuffling is working.  Now know how to handle delivery of cards. TODO: move the tracking of who has what cards to some sort of "ROUND" function. There was more, but I forget...

--- cards.js
+++ cards.js
@@ -27,8 +27,31 @@ function Card(options) {
 function Deck(options) {
     var suits = options.suits;
     var cards = options.cards;
+    /* these will be moved to a Round function but for 
+       the sake of testing I have them here as well as 
+       their accessors */
+    var northCards;
+    var eastCards;
+    var southCards;
+    var westCards;
 
     var deck = [];
+    
+    this.northHand = function() {
+        return northCards;
+    };
+    
+    this.eastHand = function() {
+        return eastCards;
+    };
+    
+    this.southHand = function() {
+        return southCards;
+    };
+    
+    this.westHand = function() {
+        return westCards;
+    }; 
     
     for(var suit in suits) {
         for(var card in cards) {
@@ -46,7 +69,22 @@ function Deck(options) {
     this.deck = function() {
         return deck;
     };
+    
+    this.deal = function() {
+        /*
+          deliver cards to each player in the form:
+          two to left three across, two to right, three to dealer
+          three to left, two across, three to right, two to dealer
+          set turned up card.  this will need to be modified to find
+          who has the deal, etc...
+        */
+        eastCards = [deck[0], deck[1], deck[10], deck[11], deck[12]];
+        southCards = [deck[2], deck[3], deck[4], deck[13], deck[14]];
+        westCards = [deck[5], deck[6], deck[15], deck[16], deck[17]];
+        northCards = [deck[7], deck[8], deck[9], deck[18], deck[19]];
+    };
 }
+
     
 
 var euchreDeck = new Deck({
